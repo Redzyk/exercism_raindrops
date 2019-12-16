f29d96e4d8121cba56926c53b6346df8ef1a7aa5
Redzyk <rodzynq@gmail.com>
2019-12-16 20:27:10 +0100

    solution two

--- raindrops.py
+++ raindrops.py
@@ -1,32 +1,17 @@
 def convert(number):
 
-    #git init
-    #git remote add origin https://github.com/Redzyk/exercism_raindrops.git
-
     result = ""
 
-    if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
-        result += "PlingPlangPlong"
-
-    elif number % 3 == 0 and number % 5 == 0:
-        result += "PlingPlang"
-
-    elif number % 3 == 0 and number % 7 == 0:
-        result += "PlingPlong"
-
-    elif number % 5 == 0 and number % 7 == 0:
-        result += "PlangPlong"
-
-    elif number % 3 == 0:
+    if number % 3 == 0:
         result += "Pling"
 
-    elif number % 5 == 0:
+    if number % 5 == 0:
         result += "Plang"
 
-    elif number % 7 == 0:
+    if number % 7 == 0:
         result += "Plong"
 
-    else:
+    elif number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
         result = str(number)
 
     return result
